a6d1ec6d531bb6f16384f27d36f2026aec10a5e5
jperomingan@startechup.com <jperomingan@startechup.com>
2021-02-04 18:03:05 +0800

    Used tailwindcss.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <h1>Dc Heros</h1>
+  <h1 class="text-center">Dc Heros {{ herosCount }}</h1>
   <ul>
-    <li v-for="(hero, index) in dcHeros" :key="index">
-      {{ index }} : {{ hero.name }}
+    <li v-for="(hero, index) in dcHeros" :key="hero.name">
+      <div>{{ hero.name }}<button v-on:click="remove(index)">X</button></div>
+    <input />
     </li>
   </ul>
-  <input :value="newHero" />
-  <button :disabled="isDisabled">Add Hero</button>
+  <form @submit.prevent="addHero">
+    <input v-model.trim="newHero" placeholder="Type Hero Name Here"/>
+    <button type="submit">Add Hero</button>
+  </form>
 </template>
 
 <script>
 export default {
+  computed: {
+      herosCount(){
+          return this.dcHeros.length;
+      },
+  },
+  methods: {
+    addHero() {
+      if (this.newHero != "") {
+          this.dcHeros.unshift({ name: this.newHero });
+          this.newHero = "";
+      }
+    },
+    remove(index) {
+      this.dcHeros = this.dcHeros.filter((hero, i) => i != index);
+  },
+},
   data() {
     return {
-      isDisabled: false,
-      newHero: "Aquaman, Reloaded",
+      newHero: "",
       dcHeros: [
         { name: "SuperGirl" },
         { name: "Flash" },
         { name: "Batman" },
         { name: "Arrow" }
-      ]
+      ],
     }
   }
 }
