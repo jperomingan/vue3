1b61273091a0ad3c699e77a7a7e01a589b2ff723
jperomingan@startechup.com <jperomingan@startechup.com>
2021-02-05 18:12:59 +0800

    calendar app

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,16 @@
 <template>
-  <h1 class="text-center">Dc Heros {{ herosCount }}</h1>
-  <ul>
-    <li v-for="(hero, index) in dcHeros" :key="hero.name">
-      <div>{{ hero.name }}<button v-on:click="remove(index)">X</button></div>
-    <input />
-    </li>
-  </ul>
-  <form @submit.prevent="addHero">
-    <input v-model.trim="newHero" placeholder="Type Hero Name Here"/>
-    <button type="submit">Add Hero</button>
-  </form>
+<AppHeader />
+<div class="w-full flex">
+  <!-- <DcHeros /> -->
+  <Calendar />
+</div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader";
+import Calendar from "./components/Calendar";
+// import DcHeros from "./components/DcHeros";
 export default {
-  computed: {
-      herosCount(){
-          return this.dcHeros.length;
-      },
-  },
-  methods: {
-    addHero() {
-      if (this.newHero != "") {
-          this.dcHeros.unshift({ name: this.newHero });
-          this.newHero = "";
-      }
-    },
-    remove(index) {
-      this.dcHeros = this.dcHeros.filter((hero, i) => i != index);
-  },
-},
-  data() {
-    return {
-      newHero: "",
-      dcHeros: [
-        { name: "SuperGirl" },
-        { name: "Flash" },
-        { name: "Batman" },
-        { name: "Arrow" }
-      ],
-    }
-  }
+  components: { AppHeader, Calendar },
 }
 </script>
